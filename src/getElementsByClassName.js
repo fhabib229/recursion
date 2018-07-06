78da324e88f601e0b2199d70295fe2787e57e3d8
Faris Habib <faris.habib.7@gmail.com>
2018-07-06 09:00:17 -0700

    Added functions for Stringify and getElementsByClassName for JSON

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,5 +6,21 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className
 ) {
-  // your code here
+  var arr = [];
+	function run(element, arr, className){
+		var mother = element.childNodes
+		for (var i = 0; i < mother.length; i++){
+			var list = mother[i].classList || [];
+			list.forEach(function(tempClass){
+				if (tempClass === className){
+					arr.push(mother[i]);
+				}
+			})
+			if (mother[i].childNodes[0]){
+				run(mother[i], arr, className);
+			}
+		}
+	}
+	run(document, arr, className);
+	return arr;
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,34 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+	if(obj === null){
+		return "" + obj;
+	}
+
+	if(typeof obj === "string"){
+		return '"' + obj + '"';
+	}
+
+	if( Array.isArray(obj) ){
+		var answer = [];
+		for(var i = 0 ; i < obj.length ; i++){
+			answer.push(stringifyJSON(obj[i]))
+		}
+		return '[' + answer + ']';
+	}
+
+	if ( !Array.isArray(obj) && typeof obj === "object"){
+		var answer = [];
+
+		for(var key in obj){
+			if(obj[key] === undefined || typeof obj[key] === "function"){
+				continue;
+			}
+			answer.push(stringifyJSON(key) + ":" + stringifyJSON(obj[key]))
+		}
+
+		return '{' + answer + '}';
+	}
+
+	return "" + obj;
 };
